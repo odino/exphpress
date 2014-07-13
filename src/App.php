c56756676bab7ab8b93fdd33544e027adb081e1c
odino <alessandro.nadalin@gmail.com>
2014-07-13 20:35:20 +0400

    phpdoc

--- src/App.php
+++ src/App.php
@@ -5,17 +5,44 @@ namespace Exphpress;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * The Exphpress application, our main
+ * interface to the world :)
+ */
 class App
 {
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
     protected $request;
+    
+    /**
+     * @var \Symfony\Component\HttpFoundation\Response
+     */
     protected $response;
     
+    /**
+     * Constructor that simply sets req/res in internal
+     * members.
+     * 
+     * @param Symfony\Component\HttpFoundation\Request $req
+     * @param Symfony\Component\HttpFoundation\Response $res
+     */
     public function __construct(Request $req, Response $res)
     {
         $this->request  = $req;
         $this->response = $res;
     }
     
+    /**
+     * Execute the $callback once a request hits the app.
+     * 
+     * Calling listen will effectively terminate the request's
+     * lifecycle, as it will start the process of sending the
+     * response to the client.
+     * 
+     * @param Closure $callback
+     */
     public function listen(\Closure $callback = null)
     {
         if ($callback) {
