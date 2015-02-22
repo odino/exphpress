420ed04a6f6344c575f153783bf2076e2fe83090
Ilpaijin <ilpaijin@gmail.com>
2015-02-22 11:31:57 +0100

    required php5.4, removed php5.3  hack in Closure

--- examples/simple.php
+++ examples/simple.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = Exphpress\app();
 
-$app->listen(function($req, $res){
+$app->listen(function($req, $res) {
     $date = new \DateTime();
-    
+
     $res->setContent("Today is " . $date->format('l, jS \o\f F Y'));
 });
